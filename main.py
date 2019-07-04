f924025bd2a7ff888a3d532092c66fbc18e5cb61
Marco Ferrari <ferrari.marco@gmail.com>
2019-07-04 18:44:21 +0200

    Use values from config for MQTT endpoint (#5)

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def get_mqtt_client(project_id, cloud_region, registry_id, device_id, jwt):
     this device. For Google Cloud IoT Core, it must be in the format below."""
     client_id = 'projects/{}/locations/{}/registries/{}/devices/{}'.format(project_id, cloud_region, registry_id, device_id)
     print('Sending message with password {}'.format(jwt))
-    client = MQTTClient(client_id.encode('utf-8'),server='mqtt.googleapis.com',port=8883,user=b'ignored',password=jwt.encode('utf-8'),ssl=True)
+    client = MQTTClient(client_id.encode('utf-8'),server=config.google_cloud_config['mqtt_bridge_hostname'],port=config.google_cloud_config['mqtt_bridge_port'],user=b'ignored',password=jwt.encode('utf-8'),ssl=True)
     return client
 
 connect()
